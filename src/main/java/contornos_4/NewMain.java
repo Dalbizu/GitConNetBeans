d9fe1a870e06ea692dea60347d083fd0ac397cd4
Dalbizu <70915352+Dalbizu@users.noreply.github.com>
2022-03-04 12:36:37 +0100

    Apartado d

--- src/main/java/contornos_4/NewMain.java
+++ src/main/java/contornos_4/NewMain.java
@@ -3,7 +3,7 @@ package contornos_4;
 public class NewMain {
 
     public static void main(String[] args) {
-        System.out.println(Sumador.sumar(15f, 4f));
+        System.out.println(Sumador.sumar(16f, 4f));
     }
     
 }
